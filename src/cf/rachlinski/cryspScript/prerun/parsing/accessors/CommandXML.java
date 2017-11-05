8ea4de65cbfe77f74f62ff14427499fd9a8383ca
chrisco210 <chrisrachlinski@gmail.com>
2017-11-05 00:40:47 -0400

    Added first version of COmmands xml file handler

---
+++ src/cf/rachlinski/cryspScript/prerun/parsing/accessors/CommandXML.java
@@ -0,0 +1,46 @@
+package cf.rachlinski.cryspScript.prerun.parsing.accessors;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class CommandXML
+{
+	/**
+	 * XML Document representing the commands to use
+	 */
+	public static Document COMMANDS_XML;
+
+
+	/*
+	TODO Make this class expandable to any number of xml files, in any location in any project.
+	 */
+	static
+	{
+		try
+		{
+			COMMANDS_XML = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder()
+					.parse(CommandXML.class.getResourceAsStream("commands.xml"));
+		}
+		catch (SAXException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ParserConfigurationException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
